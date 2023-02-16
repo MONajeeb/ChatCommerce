70a3cb8ccc7464283fd04ca6f7643128bd130a0c
fadyfayek <fady.fayek@dsquares.com>
2023-02-16 15:55:40 +0200

    twilliow changes

---
+++ ChatCommerce/Controllers/WhatsAppIntegrationController.cs
@@ -0,0 +1,50 @@
+using ChatCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+
+namespace ChatCommerce.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WhatsAppIntegrationController : Controller
+    {
+        private readonly TwilioConfig _config;
+        public WhatsAppIntegrationController(IOptions<TwilioConfig> config)
+        {
+            _config = config.Value;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(string phoneNumber, string productName)
+        {
+            try
+            {
+                TwilioClient.Init(_config.AccountSid, _config.AuthToken);
+
+                var to = new PhoneNumber("whatsapp:" + phoneNumber);
+                var from = new PhoneNumber(_config.FromNumber);
+
+                var message = $"Thank you for choosing {productName}! We have received your order and will be in touch shortly to confirm the details.";
+
+                var messageOptions = new CreateMessageOptions(to)
+                {
+                    From = from,
+                    Body = message
+                };
+
+                var response = await MessageResource.CreateAsync(messageOptions);
+
+                return Ok(response.Sid);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+        }
+       
+    }
+}
--- ChatCommerce/Program.cs
+++ ChatCommerce/Program.cs
@@ -1,3 +1,5 @@
+using ChatCommerce.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.Configure<TwilioConfig>(builder.Configuration.GetSection("Twilio"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
